65d37717c5fc57f74c3aff272652a5804c53c242
Roginth B <55707004+RoginthB@users.noreply.github.com>
2024-06-16 00:20:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const imageRoutes = require('./routes/imageRoutes');
 const createUser = require('./routes/Createuser')
 
-const app = express({limit:'50mb'});
+const app = express();
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB_CONNECTING_STRING, {
@@ -15,7 +15,7 @@ mongoose.connect(process.env.DB_CONNECTING_STRING, {
 });
 
 // Middleware
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(cors());
 // Routes
 app.use('/images', imageRoutes);

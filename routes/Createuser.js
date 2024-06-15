f1251de9c83f2f790023369d70f10c00e64300e2
Roginth B <55707004+RoginthB@users.noreply.github.com>
2024-06-15 17:24:30 +0530

    Update Createuser.js

--- routes/Createuser.js
+++ routes/Createuser.js
@@ -1,83 +1,110 @@
 // server/routes/imageRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const mongoose = require('mongoose');
-const NewUser = require('../models/newuser');
-const Image = require('../models/image');
-router.get('/login/:email', async (req, res) => {
+const multer = require("multer");
+const mongoose = require("mongoose");
+const NewUser = require("../models/newuser");
+const Image = require("../models/image");
+const bcrypt = require("bcrypt");
+router.get("/login/:email", async (req, res) => {
   const data = await NewUser.findOne({ email: req.params.email });
   res.send(data);
-})
-router.post('/newuser', async (req, res) => {
-  try {
-    const { name,
-      email,
-      password,
-      confirmPassword,
-      dateOfBirth,
-      gender,
-      followers,
-      following, post, userId } = req.body;
+});
 
-    const newuser = new NewUser({
+//login API
+router.post("/login", async(req,res)=>{
+  const user = await NewUser.findOne({email:req.body.email});
+  if(user === null){
+    res.status(205).send({message:"User not found."} )
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password )){
+      res.status(200).send(user);
+    }else{
+      res.status(203).send({message:"Wrong Password"});
+    }
+  } catch (error) {
+    res.status(500).send();
+  } 
+})
+// New user Create API
+router.post("/newuser", async (req, res) => {
+  try {
+    const {
       name,
       email,
-      password,
-      confirmPassword,
       dateOfBirth,
       gender,
       followers,
       following,
       post,
-      userId
+      userId,
+    } = req.body;
+    const password = await bcrypt.hash(req.body.password, 10);
+    const newuser = new NewUser({
+      name,
+      email,
+      password,
+      dateOfBirth,
+      gender,
+      followers,
+      following,
+      post,
+      userId,
     });
     const x = await NewUser.find({ email: newuser.email });
     if (x.length === 0) {
       await newuser.save();
-      res.status(200).json({ "message": "User is created succusfully." })
+      res.status(200).json({ message: "User is created succusfully." });
     } else {
-      res.status(400).json({ "message": "This email Id already used so you can Login " })
+      res
+        .status(400)
+        .json({ message: "This email Id already used so you can Login " });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
+});
+
+
+// Get all images API
+router.get("/images", async (req, res) => {
+  const data = await NewUser.find({});
+  const allImages = [];
+  data.map((user) => {
+    allImages.push(...user.post);
+  });
+  res.send(allImages);
+});
+
+router.put("/:id/image", async (req, res) => {
+  console.log(req.params.id);
+  const image = await NewUser.find({ post: [{ _id: req.params.id }] });
+  console.log(image);
+  await NewUser.findOneAndUpdate(
+    { post: { _id: req.params.id } },
+    { $set: { post: [{ rating: +1 }] } }
+  );
+  res.json({ message: " rated successfully." });
 });
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads/');
+    cb(null, "./public/uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
-router.get('/images', async (req, res) => {
-  const data = await NewUser.find({});
-  const allImages = [];
-  data.map( user =>{
-    allImages.push(...user.post);
-  })
-  res.send(allImages);
-})
 
-router.put('/:id/image', async (req,res)=>{
-  console.log(req.params.id)
-  const image = await NewUser.find({post:[{_id:req.params.id}]})
-  console.log(image);
-  await NewUser.findOneAndUpdate(
-  {post: {_id: req.params.id}},
-  {$set:{post:[{rating:+1}]}}
-  )
-  res.json({ "message": " rated successfully." })
-})
 const upload = multer({ storage: storage });
-// post Image 
-router.post('/:id/upload', upload.single('image'), async (req, res) => {
+// post Image
+router.post("/:id/upload", upload.single("image"), async (req, res) => {
   try {
-    const { title, description, israted, rating, userName, userId } = req.body;
-
+    const { title, description, israted, rating} = req.body;
+    const id = req.params.id;
+    const user = await NewUser.findById(id);
     const newImage = new Image({
       imageName: req.file.filename,
       imagePath: req.file.path,
@@ -85,47 +112,73 @@ router.post('/:id/upload', upload.single('image'), async (req, res) => {
       description,
       israted,
       rating,
-      userName,
-      userId
+      userName: user.name,
+      userId: user.userId,
+      userUniqId:user._id
     });
-    const imageDetails = req.body;
-    const id = req.params.id;
-    const user = await NewUser.find({ _id: id });
+    
     if (user) {
       const result = await NewUser.updateOne(
         { _id: id },
         { $push: { post: newImage } }
       );
       if (result.modifiedCount > 0) {
-        res.json({ "message": "updated successfully." })
+        res.status(200).send({ message: "updated successfully." });
       } else {
-        res.json({ "message": " not updated successfully." })
+        res.status(203).send({ message: " not updated successfully." });
       }
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
-
-router.post('/:id/post', async (req, res) => {
-  const imageDetails = req.body;
-  const id = req.params.id;
-  const user = await NewUser.find({ _id: id });
-  if (user) {
-    const result = await NewUser.updateOne(
-      { _id: id },
-      { $push: { post: imageDetails } }
-    );
-    if (result.modifiedCount > 0) {
-      res.json({ "message": "updated successfully." })
-    } else {
-      res.json({ "message": " not updated successfully." })
-    }
-  }
+// router.post("/post/:id", upload.single("image"), async (req,res)=>{
+//   const { title, description, israted, rating, userName, userId } = req.body;
+//     const newImage = new Image({
+//       imageName: req.file.filename,
+//       imagePath: req.file.path,
+//       title,
+//       description,
+//       israted,
+//       rating,
+//       userName,
+//       userId,
+//     });
+//   const user = await NewUser.findById(req.params.id);
   
-})
+//   if (user) {
+//       const result = await NewUser.updateOne(
+//         { _id: req.params.id },
+//         { $push: { post: newImage } }
+//       );
+//       if (result.modifiedCount > 0) {
+//         res.json({ message: "updated successfully." });
+//       } else {
+//         res.json({ message: " not updated successfully." });
+//       }
+//     }
+
+//     res.send(user)
+// })
+
+// router.post("/:id/post", async (req, res) => {
+//   const imageDetails = req.body;
+//   const id = req.params.id;
+//   const user = await NewUser.find({ _id: id });
+//   if (user) {
+//     const result = await NewUser.updateOne(
+//       { _id: id },
+//       { $push: { post: imageDetails } }
+//     );
+//     if (result.modifiedCount > 0) {
+//       res.json({ message: "updated successfully." });
+//     } else {
+//       res.json({ message: " not updated successfully." });
+//     }
+//   }
+// });
 
 // router.post('/:id/follower', async (req, res) => {
 //   //const user = await NewUser.find
@@ -147,7 +200,6 @@ router.post('/:id/post', async (req, res) => {
 
 // })
 
-
 // router.post('/:id/following', async (req, res) => {
 //   const user = await NewUser.find({ _id: req.params.id })
 //   const followingOrNot = false;
@@ -168,4 +220,3 @@ router.post('/:id/post', async (req, res) => {
 // })
 
 module.exports = router;
-

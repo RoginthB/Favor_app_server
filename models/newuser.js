03f88abaf54a6210a0bf8ee37ac03dce09e896ee
Roginth B <55707004+RoginthB@users.noreply.github.com>
2024-06-16 19:15:45 +0530

    Update newuser.js

--- models/newuser.js
+++ models/newuser.js
@@ -1,75 +1,73 @@
 //const Image = require('./image');
-const mongoose = require('mongoose');
+const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
 const imageSchema = new mongoose.Schema({
-	// imageName: {
-	//   type: String,
-	//   required: true,
-	// },
-	imagePath: {
-	  type: Buffer,
-	  required: false,
-	},
-	title: {
-	  type: String,
-	  required: true,
-	},
-	description: {
-	  type: String,
-	  required: true,
-	},
-	israted: {
-	  type:Boolean,
-	  required: true,
-	},
-	rating:{
-	  type:Number,
-	  required: true,
-	},
-	 userName:{
-	  type:String,
-	  required: true,
-  
-	},
-	userId:{
-	  type:String,
-	  required: true,
-	},
-	userUniqId:{
-		type:String,
-		required: true,
-	},
-  
-  });
-  
-const newUserSchema = new mongoose.Schema({
-   	name: {
-   		type:String,
-   		require:true,
-   	},
-    email:{
-   		type:String,
-   		require:true,
-   	},
-    password: {
-   		type:String,
-   		require:true,
-   	},
-    dateOfBirth: {
-   		type:Date,
-   		require:true,
-   	},
-    gender: {
-   		type:String,
-   		require:true,
-   	},
-   	followers :[{}],
-   	following :[{}],
-   	post:[imageSchema],
-   	userId:{
-   		type:String,
-   		require:true,
-   	}
+  // imageName: {
+  //   type: String,
+  //   required: true,
+  // },
+  imagePath: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  israted: {
+    type: Boolean,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  userUniqId: {
+    type: ObjectId,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('newuser', newUserSchema);
+const newUserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    require: true,
+  },
+  gender: {
+    type: String,
+    require: true,
+  },
+  followers: [{}],
+  following: [{}],
+  post: [imageSchema],
+  userId: {
+    type: String,
+    require: true,
+  },
+});
 
+module.exports = mongoose.model("newuser", newUserSchema);

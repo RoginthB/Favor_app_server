fac359626c2a38e7487001d7cab7928c180b1638
Roginth B <55707004+RoginthB@users.noreply.github.com>
2024-06-17 22:47:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ mongoose.connect(process.env.DB_CONNECTING_STRING, {
 });
 
 // Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(express.json({limit:'50mb'}));
 app.use(cors());
 // Routes
